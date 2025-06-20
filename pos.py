fe0302c68da49b2124926697348ddf9cd6e626dd
cliffamadeus <dev.clfm5t12@gmail.com>
2025-06-20 15:48:48 +0800

    Added pos.py

---
+++ pos.py
@@ -0,0 +1,7 @@
+print("Welcome to Ganja Life")
+print("-----------------------------------")
+
+print("[1] Exit Application")
+print("[2] Continue to Arithmetic Operations")
+title_choice = int(input("Enter your choice: "))
+
