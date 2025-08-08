ba63289057ca53d260a2682b658fc2c001ca3f19
cliffamadeus <dev.clfm5t12@gmail.com>
2025-08-08 20:35:40 +0800

    Added display name method in menu.py

--- menu.py
+++ menu.py
@@ -6,11 +6,16 @@ def clear_console():
 def display_title():
     print("Welcome to my application")
 
+def display_name(name):
+    print("It is nice to meet you "+name)
+
 def main():
 
     while True:
-        clear_console() 
+        clear_console()
+        name = input("Enter your name: ")
         display_title()
+        display_name(name)
         again = input("\nThere's not that much to do here, do you want to exit? y/n :").strip().lower()
         if again !='n':
             print("Lata beech")
