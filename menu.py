cbba7df38c812b3de00d8e192ac03cecbb565ce2
cliffamadeus <dev.clfm5t12@gmail.com>
2025-08-08 20:31:23 +0800

    Added display title function

--- menu.py
+++ menu.py
@@ -3,10 +3,14 @@ import os
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def display_title():
+    print("Welcome to my application")
+
 def main():
+
     while True:
         clear_console() 
-        print("Welcome to my application")
+        display_title()
         again = input("\nThere's not that much to do here, do you want to exit? y/n :").strip().lower()
         if again !='n':
             print("Lata beech")
