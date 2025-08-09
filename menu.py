8a84f34ea0fb9474344e6d139e72f5c214039625
cliffamadeus <dev.clfm5t12@gmail.com>
2025-08-09 19:43:09 +0800

    Added display number

--- menu.py
+++ menu.py
@@ -9,13 +9,28 @@ def display_title():
 def display_name(name):
     print("It is nice to meet you "+name)
 
+def display_number(number):
+    print("Oh your favorite number is " +number)
+
 def main():
 
     while True:
+
         clear_console()
         name = input("Enter your name: ")
+
+        while True:
+            number_input = input("Enter your favorite number: ")
+            try:
+                num = int(number_input)
+                break
+            except ValueError:
+                print("Invalid input, please input a valid number")
+
+
         display_title()
         display_name(name)
+        display_number(number_input)
         again = input("\nThere's not that much to do here, do you want to exit? y/n :").strip().lower()
         if again !='n':
             print("Lata beech")
